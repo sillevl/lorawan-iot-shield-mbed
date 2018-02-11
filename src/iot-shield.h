531e05b869493a37eb4e9c3145606fde76666bd5
Sille Van Landschoot <info@sillevl.be>
2018-02-11 13:41:19 +0100

    add Bosh BME280 driver and example

--- src/iot-shield.h
+++ src/iot-shield.h
@@ -4,6 +4,7 @@
 #include "drivers/C12832/C12832.h"
 #include "drivers/TLC59116/TLC59116.h"
 #include "drivers/PirSensor/PirSensor.h"
+#include "drivers/BME280/BME280.h"
 
 // Libraries
 #include "lib/Display.h"
