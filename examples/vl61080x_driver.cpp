ac4fa086e89251c96b410d5f2dacb1566d0835c6
Sille Van Landschoot <info@sillevl.be>
2018-02-11 14:30:14 +0100

    add VL6180x driver and example

---
+++ examples/vl61080x_driver.cpp
@@ -0,0 +1,22 @@
+#include "mbed.h"
+#include "iot-shield.h"
+ 
+Display lcd;
+I2C i2c(A4, A5);
+VL6180x sensor(&i2c);
+
+int main()
+{
+    sensor.initialize();
+    sensor.startContinuousOperation();
+    // sensor.printIdentification();
+    while(true) {   // this is the third thread
+        lcd.cls();
+        lcd.locate(0,0);
+        lcd.printf("Temperature : %.2f mm", sensor.getDistance());
+        lcd.locate(0,10);
+        lcd.printf("Ambient light : %.2f lux", sensor.getAmbientLight());
+        wait(1.0);
+    }
+}
+ 
